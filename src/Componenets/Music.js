66eb6013ec750ea9deb87ac0e6ac24e66a1f89cf
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-06-19 18:12:56 +0530

    styled component

--- src/Componenets/Music.js
+++ src/Componenets/Music.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import '../assets/css/music.css'
 
 export class Music extends Component {
   render() {
+    const {musicItems,active} = this.props;
     return (
-      <div>Music</div>
+      <div className="music">
+          <h3>Music</h3>
+          <ul>
+          {musicItems.map((element, index)=>{
+                      return active===index?<li key={index} className="active">&nbsp;{element}</li>:<li key={index}>&nbsp;{element}</li>
+                  })}
+          </ul>
+      </div>
     )
   }
 }
