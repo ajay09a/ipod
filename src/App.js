7c85c4cc29da98ff03dab8c6635cb48141f3b128
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-06-19 16:28:07 +0530

    import song, cover and wallpaper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Wheel from './Componenets/Wheel';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Wheel />
-    </div>
-  );
+// Import songs
+import song1 from "./assets/songs/A Girl Like You.mp3";
+import song2 from "./assets/songs/No sleep till brooklyn.mp3";
+import song3 from "./assets/songs/Do Ya Thang.mp3";
+import song4 from "./assets/songs/Rasputin.mp3";
+
+// Import song cover images
+import song1Img from "./assets/cover/A_Girl_Like_You.jpg";
+import song2Img from "./assets/cover/No_sleep_till_brooklyn.jpg";
+import song3Img from "./assets/cover/Do_ya_thang.jpg";
+import song4Img from "./assets/cover/Rasputin.jpg";
+
+// Import wallpapers
+import Wallpaper1 from "./assets/cover/wallpaper1.jpg";
+import Wallpaper2 from "./assets/cover/wallpaper1.jpg";
+import Wallpaper3 from "./assets/cover/wallpaper1.jpg";
+
+class App extends React.Component {
+  
 }
 
 export default App;
